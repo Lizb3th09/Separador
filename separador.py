966c865a09c5bfee61411abc962b3af874e626e7
Miistiik <105573310+Lizb3th09@users.noreply.github.com>
2024-06-30 16:17:40 -0700

    corregido otra vez

--- separador.py
+++ separador.py
@@ -1,18 +1,20 @@
-""" Que asepte A-z,incluyendo signos de puntacion,letras con signo y numeros."""
+# Lizbeth Barcenas Mancilla
 
-import string
+"""Separador de cadenas,que valide solo caracteres de nuestro lenguaje """
 
-
-def caracter_valido():
-    """Funcion para aseptar tipos de caracteres"""
-    caracteres_validos=string.ascii_letters + string.digits + string.punctuation + "áéíóúÁÉÍÓÚÑñüÜ"
-    return caracteres_validos
+gramatica = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+             'ñ','o','p','q','r','s','t','u','v','w','x','y','z',',','.',
+             '¿','?','!','¡',':',';','á','é','í','ó','ú','ü','ö','ë','ä','ï',
+             ' ','0','1','2','3','4','5','6','7','8','9']
 
 def separar_por_caracteres(cadena):
-    """"Funcion que separa la cadena"""
+    """Función que separa la cadena si todos los caracteres están en la gramática"""
+    for caracter in cadena:
+        if caracter.lower() not in gramatica:
+            return f"Error,caracter invalido'{caracter}'"
     return list(cadena)
 
-cadena_usuario = input("Ingrese una cadena de texto:")
+cadena_usuario = input("Ingrese una cadena de texto: ")
 
 caracteres_separados = separar_por_caracteres(cadena_usuario)
-print("Cadena separada por caracteres:", caracteres_separados)
+print("Cadena separada por caracteres: ", caracteres_separados)
